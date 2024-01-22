73b579f38ec6a0df9dd4d2f03b3c023106c0d814
Dani <daniel.sobrino.7e7@itb.cat>
2024-01-22 11:42:19 +0100

    primer commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     kotlin("jvm") version "1.9.21"
+    application
+    id("org.jetbrains.dokka") version "1.9.10"
+
 }
 
 group = "org.example"
@@ -11,6 +14,7 @@ repositories {
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+    dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.9.10")
 }
 
 tasks.test {
